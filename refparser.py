8333525f88f0b26fa49e013d98a9af49011c4868
Ahmad Alhashemi <ahmad@ahmadh.com>
2016-05-10 22:29:18 -0400

    Expanded the refparser test suite

--- refparser.py
+++ refparser.py
@@ -1,20 +1,36 @@
+class ReferenceSyntaxError(Exception):
+    """
+    Syntax error during parsing.
+    """
+    pass
+
 def parse_records(data_file, data_format):
+    """
+    Generates records in raw data from a file containing multiple records.
+    Currently only accepts the RIS file format.    
+    """
     if data_format != 'RIS':
-        raise Exception
+        raise ReferenceSyntaxError
+    
     in_record = False
     record = ''
+    
     for l in data_file:
         if l.startswith('TY  - '):
             if in_record:
-                raise Exception
+                raise ReferenceSyntaxError
             record = l
             in_record = True
         elif l.startswith('ER  - '):
             if not in_record:
-                raise Exception
+                raise ReferenceSyntaxError
             record += l
             in_record = False
             yield record
         else:
             if in_record:
                 record += l
+
+    if in_record:
+        # reached end of file with an open record
+        raise ReferenceSyntaxError
--- test_refparser.py
+++ test_refparser.py
@@ -1,32 +1,30 @@
 import unittest
-import io
-from refparser import parse_records
+from refparser import parse_records, ReferenceSyntaxError
 
 class TestRefParser(unittest.TestCase):
-    def test_ris_to_records(self):
-        ris_data = \
-"""
-TY  - JOUR
-ID  - 123456
-A1  - Cushing, Harvey
-ER  - 
+    def test_parse_ris_to_records(self):
+        with open('test_data/valid_first_record.ris', 'r') as first_record_file:
+            expected_first_record = first_record_file.read()
+            with open('test_data/valid.ris', 'r') as data_file:
+                parsed_first_record = next(parse_records(data_file, 'RIS'))
+                self.assertEqual(parsed_first_record, expected_first_record)
+    
+    def parse_invalid_ris(self, filename):
+        with open('test_data/{filename}'.format(filename=filename)) as data_file:
+            with self.assertRaises(ReferenceSyntaxError):
+                for _ in parse_records(data_file, 'RIS'): pass
+    
+    def test_parse_unclosed_first_record_ris(self):
+        self.parse_invalid_ris('unclosed_first_record.ris')
+    
+    def test_parse_unclosed_last_record_ris(self):
+        self.parse_invalid_ris('unclosed_last_record.ris')
+    
+    def test_parse_unopened_first_record_ris(self):
+        self.parse_invalid_ris('unopened_first_record.ris')
 
-Data in between
-
-TY  - JOUR
-ID  - 654321
-A1  - Hashimoto, Hakaru
-ER  - 
-"""
-        expected_first_record = \
-"""TY  - JOUR
-ID  - 123456
-A1  - Cushing, Harvey
-ER  - 
-"""
-        ris_file = io.StringIO(ris_data)
-        parsed_first_record = next(parse_records(ris_file, 'RIS'))
-        self.assertEqual(parsed_first_record, expected_first_record)
+    def test_parse_unopened_last_record_ris(self):
+        self.parse_invalid_ris('unopened_last_record.ris')
 
 if __name__ == '__main__':
     unittest.main()
