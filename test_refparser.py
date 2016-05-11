f7134dc3b6e94ff3d833a3b62a1ac9061f3a8eaa
Ahmad Alhashemi <ahmad@ahmadh.com>
2016-05-10 22:41:28 -0400

    Move RIS test data to a separate folder

--- test_refparser.py
+++ test_refparser.py
@@ -3,14 +3,14 @@ from refparser import parse_records, ReferenceSyntaxError
 
 class TestRefParser(unittest.TestCase):
     def test_parse_ris_to_records(self):
-        with open('test_data/valid_first_record.ris', 'r') as first_record_file:
+        with open('test_data/ris/valid_first_record.ris', 'r') as first_record_file:
             expected_first_record = first_record_file.read()
-            with open('test_data/valid.ris', 'r') as data_file:
+            with open('test_data/ris/valid.ris', 'r') as data_file:
                 parsed_first_record = next(parse_records(data_file, 'RIS'))
                 self.assertEqual(parsed_first_record, expected_first_record)
     
     def parse_invalid_ris(self, filename):
-        with open('test_data/{filename}'.format(filename=filename)) as data_file:
+        with open('test_data/ris/{filename}'.format(filename=filename)) as data_file:
             with self.assertRaises(ReferenceSyntaxError):
                 for _ in parse_records(data_file, 'RIS'): pass
     
