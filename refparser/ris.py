52f1c9e56011617a45db7c04f3966bf092945096
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2016-05-15 18:21:33 -0400

    Extract non-RIS specific code into BaseRecord

--- refparser/ris.py
+++ refparser/ris.py
@@ -1,10 +1,11 @@
 from .utils import cached_property
+from .base import BaseRecord
 from .exceptions import ReferenceSyntaxError, UnknownReferenceFormat
 from .normalizers import normalize_page_range, \
     normalize_text_value, \
     normalize_list_direction
 
-class RISRecord:
+class RISRecord(BaseRecord):
     @classmethod
     def parse(cls, data):
         in_record = False
@@ -30,9 +31,6 @@ class RISRecord:
             # reached end of file with an open record
             raise ReferenceSyntaxError
 
-    def __init__(self, raw_data):
-        self._raw_data = raw_data
-
     def raw_fields(self):
         'Returns a generator of tuples containing each raw fields name and value.'
         for line in self._raw_data.splitlines():
@@ -44,48 +42,8 @@ class RISRecord:
                 pass
 
     @cached_property
-    def _raw_fields_aggregate(self):
-        aggregate = {}
-        for field, value in self.raw_fields():
-            if not field in aggregate:
-                aggregate[field] = []
-            aggregate[field].append(value)
-        return aggregate
-
-    def _first_raw_aggregate(self, *fields):
-        """
-        Retruns the first non-empty list values of within one field that match
-        one of the field names passed or None if there was no match.
-        """
-        for field in fields:
-            if field in self._raw_fields_aggregate:
-                return self._raw_fields_aggregate[field]
-
-    def _first_raw_value(self, *fields):
-        """
-        Retruns the first value of the first field that matches one of the
-        field names passed or None if there was no match.
-        """
-        aggregate = self._first_raw_aggregate(*fields)
-        if aggregate:
-            return aggregate[0]
-
-    def _all_raw_values(self, *fields):
-        """
-        Returns all values that match any of the passed fields or None if no values
-        were found. The values are ordered by the order of the passed field names first
-        then by the order in which they appear in the record.
-        """
-        values = []
-        for field in fields:
-            if field in self._raw_fields_aggregate:
-                values += self._raw_fields_aggregate[field]
-        if values: return values
-
-    @cached_property
     def title(self):
         return self._first_raw_value('TI', 'T1')
-
 
     @cached_property
     def abstract(self):
@@ -133,51 +91,3 @@ class RISRecord:
     @cached_property
     def end_page(self):
         return self._pages[1]
-
-    @cached_property
-    def authors_lastnames(self):
-        def guess_lastname(author):
-            if ',' in author:
-                # Lastname, F. or Lastname, Firstname
-                return author.split(',', 1)[0]
-            elif author.endswith('.'):
-                # Lastname F.
-                return author.split(' ', 1)[0]
-            else:
-                # Firstname Lastname
-                return author.rsplit(' ', 1)[-1]
-
-        return [guess_lastname(author) for author in self.authors]
-
-    @cached_property
-    def location_fingerprint(self):
-        """
-        Returns a fingerprint of the record containing the journals ISSN,
-        volume, issue and pages. Returns None if any of this data is missing.
-        """
-        if None in (self.start_page, self.issue, self.volume, self.issn):
-            return None
-
-        return '$'.join((
-                (normalize_page_range(self.start_page, self.end_page)),
-                self.volume,
-                self.issue,
-                self.issn,))
-
-    @cached_property
-    def title_authors_fingerprint(self):
-        """
-        Returns a fingerprint of the record composed of the title and list of
-        authors lastnames. Authors lastnames are listed alphabetically to
-        keep them canonical as some records list the authors in a reverse order.
-        """
-        if None in (self.title, self.authors_lastnames):
-            return None
-
-        lastnames = list(map(normalize_text_value, self.authors_lastnames))
-        lastnames = normalize_list_direction(lastnames)
-        lastnames = '.'.join(lastnames)
-
-        title = normalize_text_value(self.title)
-
-        return '$'.join((lastnames, title))
